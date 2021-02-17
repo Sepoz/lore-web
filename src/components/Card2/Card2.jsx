5875ec8537ac24e12bf195cf73856cd2a7f845c8
Simone Boccolari <simone.boccolari@hotmail.it>
2021-02-17 14:02:49 +0100

    fixed camera

--- src/components/Card2/Card2.jsx
+++ src/components/Card2/Card2.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Canvas } from "react-three-fiber";
-import { OrbitControls, Box, useTexture, RoundedBox } from "@react-three/drei";
+import { OrbitControls, Box, useTexture } from "@react-three/drei";
 
 import "./Card2.css";
 
 import cardBack from "../../img/CardBack.jpeg";
-import darioMoccia from "../../img/DarioMoccia.jpeg";
 
 const Card2 = (props) => {
+    let front = `${process.env.PUBLIC_URL}${props.cardImg}`;
+
     const texture1 = useTexture(cardBack);
-    const texture2 = useTexture(darioMoccia);
+    const texture2 = useTexture(front);
 
     return (
         <div className="canvas">
@@ -17,12 +18,12 @@ const Card2 = (props) => {
                 pixelRatio={window.devicePixelRatio}
                 camera={{ position: [0.8, 0, 2] }}
             >
-                <OrbitControls />
+                <OrbitControls enableZoom={false} />
                 <ambientLight intensity={0.5} />
-                <Box args={[1.3, 2, 0.02]}>
+                <Box args={[1.3, 2, 0.005]}>
                     <meshBasicMaterial attach="material" map={texture1} />
                 </Box>
-                <Box args={[1.3, 2, 0.001]} position={[0, 0, 0.01]}>
+                <Box args={[1.3, 2, 0.001]} position={[0, 0, 0.005]}>
                     <meshBasicMaterial attach="material" map={texture2} />
                 </Box>
             </Canvas>
